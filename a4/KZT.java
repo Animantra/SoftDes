6e9dc5655f30d2e167b5ba087f322f2335be210d
Animantra <t20227893@gmail.com>
2025-10-02 12:18:19 +0500

    Add files via upload

---
+++ a4/KZT.java
@@ -0,0 +1,7 @@
+public class KZT implements iCurrency {
+    public void convert(int amount){
+        System.out.println(amount + " " + "Tenge");
+
+    }
+    
+}
---
+++ a4/OfflinePay.java
@@ -0,0 +1,14 @@
+public class OfflinePay extends Payment{
+    public OfflinePay(iCurrency currency){
+        super(currency);
+
+    }
+
+     @Override
+    public void pay(int amount) {
+        System.out.println("InStore payment");
+        System.out.println("Written off :");
+        currency.convert(amount);
+    }
+    
+}
