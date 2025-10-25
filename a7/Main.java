986297d5c05b8750ac2d755008d2996f1761ad6f
Animantra <t20227893@gmail.com>
2025-10-25 19:28:14 +0500

    Delete a7 directory

--- a7/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Student student = new Student("Denis", new standardGrade());
-        System.out.println(student.calculate(85));
-
-        student.setStrategy(new resultGrade());
-        System.out.println(student.calculate(40));
-    }
-}
--- a7/iGradeStrategy.java
+++
@@ -1,3 +0,0 @@
-public interface iGradeStrategy{
-    String calcGrade(float finalScore);
-}
