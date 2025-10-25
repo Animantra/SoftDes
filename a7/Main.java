4c3782d51f3ecbb4958ebc2a8db285d5b358aa5d
Animantra <t20227893@gmail.com>
2025-10-25 19:28:53 +0500

    Add files via upload

---
+++ a7/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("Denis", new standardGrade());
+        System.out.println(student.calculate(85));
+
+        student.setStrategy(new resultGrade());
+        System.out.println(student.calculate(40));
+    }
+}
