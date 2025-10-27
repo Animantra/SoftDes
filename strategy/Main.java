66ef0583d615a85ab2d7d5c6eccfe8f5fffbbb4c
Animantra <t20227893@gmail.com>
2025-10-27 10:19:34 +0500

    upd2

--- strategy/Main.java
+++ strategy/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Student student = new Student("Denis");
+        
         student.setStrategy(new standardGrade());
         System.out.println(student.calculate(85));
 
